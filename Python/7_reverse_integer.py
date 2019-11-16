2614f335e9bb4255a77ba42c72a23113041867a0
Grace Nellore <gracernellore@gmail.com>
2019-11-16 10:50:18 -0800

    handle integer overflows

--- Python/7_reverse_integer.py
+++ Python/7_reverse_integer.py
@@ -1,17 +1,28 @@
+"""
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+"""
 class Solution:
     def reverse(self, x: int) -> int:
         sign = 1                        # Sign initialized to positive
-        
+
         if x < 0:                       # If x less than 0
             sign = -1                   # Update sign to negative
             x *= -1                     # Update x to positive so that we can use while x > 0 condition
-        
+
         res = 0                         # Build res from 0
-        
+
         while x > 0:                    # While digits remaining in x
             res = res * 10 + x % 10     # Add last digit to the back of res
             x //= 10                    # Remove last digit of x
-            
-        return res * sign               # Return res updated with correct staring sign
-    
-        
+
+        res *= sign                     # Apply sign on res
+
+        # Need to handle overflow,  [−2^31,  2^31 − 1]
+        int_max = 2 ** 31 - 1
+        int_min = -(2 ** 31)
+
+        if res < int_min or res > int_max:
+            return 0
+
+        return res
