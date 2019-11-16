4ea3067b31e3ae29bddf5dbeaeeb2a0973e4c3fa
Grace Nellore <gracernellore@gmail.com>
2019-11-16 10:37:46 -0800

    need to update negative input to positive versino in order to use x<0 for while condition

--- Python/7_reverse_integer.py
+++ Python/7_reverse_integer.py
@@ -1,8 +1,12 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        # If x is negative, return res * negative one
-        sign = -1 if x < 0 else 1
-        res = 0
+        sign = 1                        # Sign initialized to positive
+        
+        if x < 0:                       # If x less than 0
+            sign = -1                   # Update sign to negative
+            x *= -1                     # Update x to positive so that we can use while x > 0 condition
+        
+        res = 0                         # Build res from 0
         
         while x > 0:                    # While digits remaining in x
             res = res * 10 + x % 10     # Add last digit to the back of res
